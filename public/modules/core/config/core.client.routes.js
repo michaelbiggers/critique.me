051e6a649283f21e24ec7db97802a98dc348c7bb
michaelbiggers <mbiggers@knights.ucf.edu>
2015-04-18 18:35:02 -0400

    views in place
    Added view/route for feed page

--- public/modules/core/config/core.client.routes.js
+++ public/modules/core/config/core.client.routes.js
@@ -11,6 +11,10 @@ angular.module('core').config(['$stateProvider', '$urlRouterProvider',
 		state('home', {
 			url: '/',
 			templateUrl: 'modules/core/views/home.client.view.html'
+		}).
+		state('imageFeed', {
+			url: '/feed',
+			templateUrl: 'modules/core/views/imageFeed.client.view.html'
 		});
 	}
 ]);
