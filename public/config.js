2bc18c344a0dcfb298ef16d1d40dec7829134e80
michaelbiggers <mbiggers@knights.ucf.edu>
2015-05-04 00:42:34 -0400

    angular x-editable
    breaks the view-profile page, but does live update text. Doesn’t save
    updated text from xeditable box, but does save from edit profile

--- public/config.js
+++ public/config.js
@@ -4,7 +4,7 @@
 var ApplicationConfiguration = (function() {
 	// Init module configuration options
 	var applicationModuleName = 'mean';
-	var applicationModuleVendorDependencies = ['ngResource', 'ngAnimate', 'ui.router', 'ui.bootstrap', 'ui.utils', 'angucomplete', 'monospaced.elastic'];
+	var applicationModuleVendorDependencies = ['ngResource', 'ngAnimate', 'ui.router', 'ui.bootstrap', 'ui.utils', 'angucomplete', 'monospaced.elastic','xeditable'];
 
 	// Add a new vertical module
 	var registerModule = function(moduleName, dependencies) {
--- public/modules/users/controllers/settings.client.controller.js
+++ public/modules/users/controllers/settings.client.controller.js
@@ -3,6 +3,7 @@
 angular.module('users').controller('SettingsController', ['$scope', '$http', '$location', 'Users', 'Authentication',
 	function($scope, $http, $location, Users, Authentication) {
 		$scope.user = Authentication.user;
+		
 
 		// If user is not signed in then redirect back home
 		if (!$scope.user) $location.path('/');
@@ -66,6 +67,9 @@ angular.module('users').controller('SettingsController', ['$scope', '$http', '$l
 			}).error(function(response) {
 				$scope.error = response.message;
 			});
+		};
+		$scope.user = {
+			userDescription: user.userDescription
 		};
 	}
 ]);
